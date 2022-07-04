bc20b958399bec2b05637bc8c9fedddccffc9267
Paul Schaub <vanitasvitae@fsfe.org>
2022-07-04 18:48:49 +0200

    Make trust-root constant in SpecialNames

--- pgp-cert-d-java/src/main/java/pgp/cert_d/SpecialNames.java
+++ pgp-cert-d-java/src/main/java/pgp/cert_d/SpecialNames.java
@@ -9,11 +9,14 @@ import java.util.Map;
 
 public class SpecialNames {
 
+    public static final String TRUST_ROOT = "trust-root";
+
+    // Map to allow for potentially upper- and lowercase variants of the same special name
     private static final Map<String, String> SPECIAL_NAMES = new HashMap<>();
 
     static {
-        SPECIAL_NAMES.put("TRUST-ROOT", "trust-root"); // TODO: Remove
-        SPECIAL_NAMES.put("trust-root", "trust-root");
+        SPECIAL_NAMES.put("TRUST-ROOT", TRUST_ROOT); // TODO: Remove
+        SPECIAL_NAMES.put(TRUST_ROOT, TRUST_ROOT);
     }
 
     public static String lookupSpecialName(String specialName) {
